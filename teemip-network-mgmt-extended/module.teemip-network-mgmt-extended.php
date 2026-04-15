a95cb0e753bbafffc3a6965dd53ad872cd35f41e
xtophe38 <christophe.naud@gmail.com>
2026-04-15 15:36:29 +0200

    Increase revision #

--- teemip-network-mgmt-extended/module.teemip-network-mgmt-extended.php
+++ teemip-network-mgmt-extended/module.teemip-network-mgmt-extended.php
@@ -6,7 +6,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'teemip-network-mgmt-extended/3.3.0',
+	'teemip-network-mgmt-extended/3.3.1',
 	array(
 		// Identification
 		//
